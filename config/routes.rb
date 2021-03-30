c9808e20639ee8b49032c42028015913a8f22f49
Alexandre Trindade <alexandredct@gmail.com>
2021-03-30 12:38:39 -0300

    Find an article by id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #For Home Page
   root "articles#index"
 
+  #Default for Article Controller
   get "/articles", to: "articles#index"
+
+  #Show a specific article
+  get "/articles/:id", to: "articles#show"
+
 end
