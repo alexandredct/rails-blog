6a468c03f0f29aec83f59bc856e9f42d60c3c188
Alexandre Trindade <alexandredct@gmail.com>
2021-03-30 13:04:41 -0300

    Creating new and create methods for ArticleController. Using the form_helper for building forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,25 @@
 class ArticlesController < ApplicationController
   def index
-  	@articles = Article.all
+    @articles = Article.all
   end
 
   def show
-  	@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+    	#redirect_to will cause the browser to make a new request. Use after mutating the database ou application state
+      redirect_to @article
+    else
+    	#render renders the specified view for the current request.
+      render :new
+    end
   end
 end
