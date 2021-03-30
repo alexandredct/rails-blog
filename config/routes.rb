249058d6020636fc221b8181afcbcbd17625b48e
Alexandre Trindade <alexandredct@gmail.com>
2021-03-30 12:54:03 -0300

    Using "link_to" method

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   #Whenever we have such a combination of routes, controller actions, and views that work together to perform CRUD operations on an entity, we call that entity a resource.
   #@see https://api.rubyonrails.org/v6.1.3.1/classes/ActionDispatch/Routing/Mapper/Resources.html#method-i-resources
+  #@see https://guides.rubyonrails.org/routing.html
   resources :articles
 
 end
